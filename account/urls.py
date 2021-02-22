7f37d66687e40d118237c2628866451f66ccd0fa
mohammadanarul <mdanarul7075@gmail.com>
2021-02-22 23:04:52 +0600

    initial.

--- account/urls.py
+++ account/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from account.views import (
     user_register_page,
     user_login_page,
-    user_logout_page
+    user_logout_page,
+    user_dashboard_page_view
 )
 
 app_name = 'account'
@@ -10,4 +11,5 @@ urlpatterns = [
     path('user-register/', user_register_page, name='user_register'),
     path('user-login/', user_login_page, name='user_login'),
     path('user-logout/', user_logout_page, name='user_logout'),
+    path('@<str:username>/', user_dashboard_page_view, name='user_dashboard'),
 ]
--- account/views.py
+++ account/views.py
@@ -2,14 +2,20 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from account.forms import user_register_form
 
-def user_register_page(request):
+def user_register_page(request, *args, **kwargs):
+    form = user_register_form()
     if request.method == 'POST':
         form = user_register_form(request.POST or None)
         if form.is_valid():
-            form.save()
-            return redirect('/')
+            form1 = form.save()
+            login(request, form1, backend='django.contrib.auth.backends.ModelBackend')
+            return redirect('account:user_dashboard')
+    else:
+        form = user_register_form()
+    template_name = 'account/register.html'
+    return render(request, template_name, {'form': form})
 
-def user_login_page(request):
+def user_login_page(request, *args, **kwargs):
     if request.method == 'POST':
         email = request.POST['email']
         password = request.POST['password']
@@ -17,8 +23,14 @@ def user_login_page(request):
         if user is not None:
             login(request, user)
             return redirect('/')
+    template_name = 'account/login.html'
+    return render(request, template_name)
 
 def user_logout_page(request):
     logout(request)
     return redirect('/')
 
+
+def user_dashboard_page_view(request, username):
+    template_name = 'account/dashboard.html'
+    return render(request, template_name)

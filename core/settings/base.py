94eabaaa545b85024fdb07d68c20d762b7196c5e
mohammadanarul <mdanarul7075@gmail.com>
2021-10-10 12:46:35 +0600

    add staticfiles in eshop project.

--- core/settings/base.py
+++ core/settings/base.py
@@ -106,7 +106,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'cdn_static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
--- core/settings/production.py
+++ core/settings/production.py
@@ -17,26 +17,18 @@ django_heroku.settings(locals())
 # whitenoise collectstatic
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# EMAIL_HOST = config('EMAIL_HOST')
-# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_BACKEND = config('EMAIL_BACKEND')
-# DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
-
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'mdanarul7075@gmail.com'
-EMAIL_HOST_PASSWORD = 'ydaqqzkuvbwovopf'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-DEFAULT_FROM_EMAIL = 'mdanarul7075@gmail.com'
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 
 # cloudinary config  for heroku
 # image cloud services
 cloudinary.config( 
-  cloud_name = "bdonlineshop", 
-  api_key = "282249648444274", 
-  api_secret = "6uvJ5DlJEhnfLiRh6NPTdbxQbRo" 
+  cloud_name = config('cloud_name'),
+  api_key = config('api_key'), 
+  api_secret = config('api_secret')
 )
